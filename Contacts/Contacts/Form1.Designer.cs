7869281715b231bcbf9a7349a1222be86a119441
Mario0345 <ogaskoyn@gmail.com>
2021-04-22 17:24:31 +0500

    Add contact

---
+++ Contacts/Contacts/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Contacts
+{
+    partial class From1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.surname = new System.Windows.Forms.TextBox();
+            this.position = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(12, 27);
+            this.name.Name = "name";
+            this.name.PlaceholderText = "Name";
+            this.name.Size = new System.Drawing.Size(100, 23);
+            this.name.TabIndex = 1;
+            // 
+            // surname
+            // 
+            this.surname.Location = new System.Drawing.Point(145, 27);
+            this.surname.Name = "surname";
+            this.surname.PlaceholderText = "Surname";
+            this.surname.Size = new System.Drawing.Size(100, 23);
+            this.surname.TabIndex = 1;
+            // 
+            // position
+            // 
+            this.position.Location = new System.Drawing.Point(12, 90);
+            this.position.Name = "position";
+            this.position.PlaceholderText = "Position";
+            this.position.Size = new System.Drawing.Size(100, 23);
+            this.position.TabIndex = 1;
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(12, 146);
+            this.phone.Name = "phone";
+            this.phone.PlaceholderText = "Phone number";
+            this.phone.Size = new System.Drawing.Size(153, 23);
+            this.phone.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(343, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(455, 424);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Text = "dataGridView1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // From1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(807, 458);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.position);
+            this.Controls.Add(this.surname);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.button1);
+            this.Name = "From1";
+            this.Text = "контакты";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox surname;
+        private System.Windows.Forms.TextBox position;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
